5acf3e0daf48e5aaad551a8bd6a7c531c57d7a87
Sheila Donnelly <sheila.donnelly@outlook.com>
2022-10-21 18:35:24 -0700

    Atlas connection troubleshooting

--- config/config.js
+++ config/config.js
@@ -7,4 +7,4 @@ October 7th, 2022
 
 export const Secret = "MySecret";
 //export const MongoURI = "mongodb://localhost/media";       //Mongo connection URL
-export const MongoURI = "mongodb+srv://user2:test123@webappsdev-comp229.jp7dah2.mongodb.net/media?retryWrites=true&w=majority";
+export const MongoURI = "mongodb+srv://user2:User123@fortune2.vz5ls3f.mongodb.net/media?retryWrites=true&w=majority";
